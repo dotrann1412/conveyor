4237d6ad9ddc52c6451dfdc0817ad828833bfbb5
c4856f165a6b08ba <0c3f8660989091a2>
2026-04-29 00:34:21 +0700

    update light on ocr example

--- examples/light_on_ocr_pipeline.py
+++ examples/light_on_ocr_pipeline.py
@@ -37,9 +37,7 @@ def make_generation_stage(cuda_index: int | None = None):
     model = LightOnOcrForConditionalGeneration.from_pretrained("lightonai/LightOnOCR-2-1B-bbox", torch_dtype=dtype).to(device)
     processor = LightOnOcrProcessor.from_pretrained("lightonai/LightOnOCR-2-1B-bbox")
 
-    @torch.no_grad()
-    def generate(req: dict[str, Any]):
-        # this can be in a separate stage, maybe the preprocess
+    def preprocess(req: dict[str, Any]):
         inputs = processor.apply_chat_template(
             req.pop('messages'),
             add_generation_prompt=True,
@@ -47,6 +45,12 @@ def make_generation_stage(cuda_index: int | None = None):
             return_dict=True,
             return_tensors="pt",
         )
+
+        return {"inputs": inputs, **req}
+
+    @torch.no_grad()
+    def generate(req: dict[str, Any]):
+        inputs = req["inputs"]
 
         inputs = {
             k: v.to(device=device, dtype=dtype) 
@@ -56,8 +60,12 @@ def make_generation_stage(cuda_index: int | None = None):
         }
 
         output_ids = model.generate(**inputs, max_new_tokens=1024)
-        
-        # this can be located in the post process phase
+
+        return {"output_ids": output_ids, **req}
+
+    def postprocess(req: dict[str, Any]):
+        output_ids = req.pop("output_ids")
+        inputs = req.pop("inputs")
         generated_ids = output_ids[0, inputs["input_ids"].shape[1]:]
         output_text = processor.decode(generated_ids, skip_special_tokens=True)
 
@@ -66,7 +74,7 @@ def make_generation_stage(cuda_index: int | None = None):
             "output": output_text,
         }
 
-    return generate
+    return [preprocess, generate, postprocess]
 
 async def initialize_stage(req: dict[str, Any]):
     image_url = req.get("image_url") 
@@ -114,8 +122,6 @@ async def initialize_stage(req: dict[str, Any]):
     return {"messages": messages, **req}
 
 def finalize_stage(req: dict[str, Any]):
-    # parse the text output here, 
-    # and/or render it directly into the image and response
     return {
         "output": req["output"].strip()
     }
@@ -126,10 +132,14 @@ from conveyor import Pipeline, Stage
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    model_pre, model_gen, model_post = make_generation_stage()
+
     async with Pipeline(
         stages=[
             Stage([initialize_stage] * 8, queue_size_per_worker=100),
-            Stage([make_generation_stage()], queue_size_per_worker=100),
+            Stage([model_pre], queue_size_per_worker=100),
+            Stage([model_gen], queue_size_per_worker=100),
+            Stage([model_post], queue_size_per_worker=100),
             Stage([finalize_stage], queue_size_per_worker=100),
         ]
     ) as app.state.pipeline:
